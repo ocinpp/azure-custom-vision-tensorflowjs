3ea8f48275f898b53c5592f905462a33669732f2
ocinpp <ocin25@gmail.com>
2019-02-10 21:46:43 +0800

    Add functions for camera
    Accessing camera and using user media

---
+++ scripts/camera.js
@@ -0,0 +1,44 @@
+
+async function getMediaStream(constraints) {
+  try {
+    mediaStream =  await navigator.mediaDevices.getUserMedia(constraints);
+    let video = document.getElementById('cam');
+    video.srcObject = mediaStream;
+    video.onloadedmetadata = (event) => {
+      video.play();
+    };
+  } catch (err)  {
+    console.error(err.message);
+  }
+  return mediaStream;
+}
+
+async function stopCamera(mediaStream) {
+  try {
+    // stop the current video stream
+    if (mediaStream != null && mediaStream.active) {
+      var tracks = mediaStream.getVideoTracks();
+      tracks.forEach(track => {
+        track.stop();
+      })
+    }
+
+    // set the video source to null
+    document.getElementById('cam').srcObject = null;
+  } catch (err)  {
+    console.error(err.message);
+    alert(err.message);
+  }
+}
+
+async function switchCamera(constraints) {
+  try {
+    // get new media stream
+    mediaStream = await getMediaStream(constraints);
+  } catch (err)  {
+    console.error(err.message);
+    alert(err.message);
+  }
+
+  return mediaStream;
+}
